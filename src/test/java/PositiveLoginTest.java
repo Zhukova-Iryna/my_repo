d7ea454d101562fb1f8499012702a8a34f8c3b0c
IRYNA <bu9ira@gmail.com>
2020-02-17 11:20:35 +0200

    some changes to first commit

--- src/test/java/PositiveLoginTest.java
+++ src/test/java/PositiveLoginTest.java
@@ -1,4 +1,6 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -11,7 +13,7 @@ public class PositiveLoginTest extends BaseTest {
         driver.findElement(By.id("LoginForm_username")).sendKeys("bu9ira@gmail.com");
         driver.findElement(By.id("login_user_password")).sendKeys("test2010");
         driver.findElement(By.xpath("//div[contains(@class,'pt_20')]")).click();
-        Assert.assertEquals(driver.findElement(By.xpath("//div[contains(@class,'link for-login')]")).getText(), "bu9ira");
+        Assert.assertEquals(driver.findElement(By.id("header-user-link")).getText(), "bu9ira");
         driver.quit();
     }
 }
